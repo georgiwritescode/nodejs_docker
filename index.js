a8c7ce03a772987cdfd58b8d37d1f5adbe163354
Georgi Ivanov <g.ivanov@blubito.com>
2021-06-22 22:38:59 +0300

    create&setup docker-compose file

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
-client.set('visits', 1);
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+client.set('visits', 0);
 
 app.get('/', (req, res)=>{
     client.get('visits', (err, visits)=> {
-        res.send('Visits: ', + visits);
-        client.set('visits', parseInt(visits + 1));
-    })
-    res.send('Hello there')
+        let output = 'Visits: ' + visits;
+        res.status(200).send(output);
+        client.set('visits', parseInt(visits) + 1);
+    });
 });
 
 
-app.listen(8080, ()=>{
-    console.log('Listening on port 8080...');
+app.listen(8081, ()=>{
+    console.log('Listening on port 8081...');
 });
