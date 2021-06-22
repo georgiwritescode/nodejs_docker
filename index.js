9a364353de56e547737c3d764ce1f4bc58888a49
Georgi Ivanov <g.ivanov@blubito.com>
2021-06-22 21:27:56 +0300

    js file setup

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const redis = require('redis');
 
 const app = express();
+const client = redis.createClient();
+client.set('visits', 1);
 
 app.get('/', (req, res)=>{
+    client.get('visits', (err, visits)=> {
+        res.send('Visits: ', + visits);
+        client.set('visits', parseInt(visits + 1));
+    })
     res.send('Hello there')
 });
 
 
 app.listen(8080, ()=>{
-    console.log('Listening on port 8080');
+    console.log('Listening on port 8080...');
 });
